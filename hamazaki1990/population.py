030ce0808c2214a9a2c6b97f884cb0ec7b4e5bdd
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2017-04-25 18:49:01 +0900

    Merge pull request #11 from hamazaki1990/master
    * Merge remote-tracking branch 'upstream/master'
    * new generation roop #9
    * list comprehensions

--- hamazaki1990/population.py
+++ hamazaki1990/population.py
@@ -1,14 +1,21 @@
+import random
 from individual import Individual
 
 
 class Population:
     def __init__(self, size):
-        self._individuals = [Individual(x) for x in range(size)]
+        self._inds = [Individual(x) for x in range(size)]
 
     def print_ids(self):
-        print(self._individuals)
+        print(self._inds)
+
+    def next_generation(self):
+        size = len(self._inds)
+        self._inds = [self._inds[random.randrange(size)] for x in range(size)]
 
 
-p1 = Population(10)
-print(p1._individuals)
-p1.print_ids()
+p = Population(10)
+
+for t in range(20):
+    p.print_ids()
+    p.next_generation()
