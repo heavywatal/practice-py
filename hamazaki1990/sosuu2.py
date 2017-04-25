5f49d5c3678955776768243940fcc6b503d840d6
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2017-04-25 19:36:14 +0900

    Update sosuu2.py

--- hamazaki1990/sosuu2.py
+++ hamazaki1990/sosuu2.py
@@ -1,12 +1,12 @@
 def is_prime(x):
     for a in range(2, x):
         if x % a == 0:
-            break
+            return False
     else:
-        return "true"
+        return True
 
 
 N = 1000
 for n in range(2, N+1):
-    if is_prime(n) == "true":
+    if is_prime(n):
         print(n)
