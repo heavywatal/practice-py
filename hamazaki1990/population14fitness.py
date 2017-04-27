14b51fa310d286578bb1f930250f4f784dfdbe6d
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2017-04-27 23:14:39 +0900

    add fitness

--- hamazaki1990/population14fitness.py
+++ hamazaki1990/population14fitness.py
@@ -3,8 +3,13 @@ from individual import Individual
 
 
 class Population:
-    def __init__(self, size):
-        self._inds = [Individual(x) for x in range(size)]
+    def __init__(self, size, mt=0, s=0):
+        self._mfit = 1 + s
+        self._wnum = int(size*(1-mt))
+        self._mnum = int(size*mt)
+        self._winds = [Individual("A") for x in range(self._wnum)]
+        self._minds = [Individual("a", self._mfit) for x in range(self._mnum)]
+        self._inds = self._minds + self._winds
 
     def print_ids(self):
         print(self._inds)
@@ -18,14 +23,5 @@ class Population:
         self._inds[random.randrange(n)] = self._inds[random.randrange(n)]
 
 
-p1 = Population(10)
-
-for t in range(20):
-    p1.print_ids()
-    p1.next_genwf()
-
-p2 = Population(10)
-
-for t in range(20):
-    p2.print_ids()
-    p2.next_genmo()
+p1 = Population(10, 0.3, -0.2)
+p1.print_ids()
