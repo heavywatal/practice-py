a70b1dbf973d36fa31dc50784d7c4e199073733a
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2017-05-02 21:41:27 +0900

    Reduce calculation and rename variables

--- hamazaki1990/population.py
+++ hamazaki1990/population.py
@@ -2,20 +2,18 @@ import random
 from individual import Individual
 
 
-def roulettechoice(seq1, seq2):
-    r = random.random()
-    for a in range(len(seq2)):
-        if r < seq2[a]:
-            break
-    return seq1[a]
+def roulettechoice(individuals, cumsum_fitness):
+    r = random.uniform(0.0, max(cumsum_fitness))
+    for i in range(len(cumsum_fitness)):
+        if r < cumsum_fitness[i]:
+            return individuals[i]
 
 
 class Population:
     def __init__(self, n, mutantrate=0, s=0):
-        num_wild = int(n*(1-mutantrate))
-        num_mutant = int(n*mutantrate)
+        num_mutant = int(n * mutantrate)
         mutant_inds = [Individual(x, 1 + s) for x in range(num_mutant)]
-        wild_inds = [Individual(x + num_mutant) for x in range(num_wild)]
+        wild_inds = [Individual(x) for x in range(num_mutant, n)]
         self._inds = mutant_inds + wild_inds
 
     def print_ids(self):
@@ -28,17 +26,18 @@ class Population:
     def next_genwf(self):
         fitness = [x.get_fitness() for x in self._inds]
         size = len(self._inds)
-        s_fit = [sum(fitness[0: x + 1])/sum(fitness) for x in range(size)]
+        cumsum_fitness = [sum(fitness[:i]) for i in range(1, size + 1)]
         next_generation = []
         for x in range(size):
-            next_generation.append(roulettechoice(self._inds, s_fit))
+            next_generation.append(roulettechoice(self._inds, cumsum_fitness))
         self._inds = next_generation
 
     def next_genmo(self):
         fitness = [x.get_fitness() for x in self._inds]
         size = len(self._inds)
-        s_fit = [sum(fitness[0: x + 1])/sum(fitness) for x in range(size)]
-        self._inds[random.randrange(size)] = roulettechoice(self._inds, s_fit)
+        cumsum_fitness = [sum(fitness[:i]) for i in range(1, size + 1)]
+        i_dying = random.randrange(size)
+        self._inds[i_dying] = roulettechoice(self._inds, cumsum_fitness)
 
 
 p1_1 = Population(10)
