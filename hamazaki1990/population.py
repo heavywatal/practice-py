867a00db180bcd887ef509be07b1772829f16578
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2017-05-02 19:13:46 +0900

    solved #14

--- hamazaki1990/population.py
+++ hamazaki1990/population.py
@@ -2,6 +2,14 @@ import random
 from individual import Individual
 
 
+def roulettechoice(seq1, seq2):
+    r = random.random()
+    for a in range(len(seq2)):
+        if r < seq2[a]:
+            break
+    return seq1[a]
+
+
 class Population:
     def __init__(self, n, mutantrate=0, s=0):
         num_wild = int(n*(1-mutantrate))
@@ -20,38 +28,52 @@ class Population:
     def next_genwf(self):
         fitness = [x.get_fitness() for x in self._inds]
         size = len(self._inds)
-        w = sum(fitness)
+        s_fit = [sum(fitness[0: x + 1])/sum(fitness) for x in range(size)]
         next_generation = []
-        for y in range(size):
-            r = random.random()
-            for x in range(size):
-                if sum(fitness[0: x])/w <= r < sum(fitness[0: x + 1])/w:
-                    next_generation.append(self._inds[x])
+        for x in range(size):
+            next_generation.append(roulettechoice(self._inds, s_fit))
         self._inds = next_generation
 
     def next_genmo(self):
-        fit = [x.get_fitness() for x in self._inds]
+        fitness = [x.get_fitness() for x in self._inds]
         size = len(self._inds)
-        w = sum(fit)
-        for x in range(size):
-            if sum(fit[0: x])/w <= random.random() < sum(fit[0: x + 1])/w:
-                self._inds[random.randrange(size)] = self._inds[x]
+        s_fit = [sum(fitness[0: x + 1])/sum(fitness) for x in range(size)]
+        self._inds[random.randrange(size)] = roulettechoice(self._inds, s_fit)
 
 
-p1 = Population(10, 0.3, 0.2)
-p1.print_ids()
-p1.print_fitness()
+p1_1 = Population(10)
+p1_1.print_ids()
+p1_1.print_fitness()
 
 for x in range(20):
-    p1.next_genwf()
-    p1.print_ids()
-p1.print_fitness()
+    p1_1.next_genwf()
+    p1_1.print_ids()
+p1_1.print_fitness()
 
-p2 = Population(10, 0.3, 0.2)
-p2.print_ids()
-p2.print_fitness()
+p1_2 = Population(10, 0.3, 0.2)
+p1_2.print_ids()
+p1_2.print_fitness()
 
 for x in range(20):
-    p2.next_genmo()
-    p2.print_ids()
-p2.print_fitness()
+    p1_2.next_genwf()
+    p1_2.print_ids()
+p1_2.print_fitness()
+
+
+p2_1 = Population(10)
+p2_1.print_ids()
+p2_1.print_fitness()
+
+for x in range(20):
+    p2_1.next_genmo()
+    p2_1.print_ids()
+p2_1.print_fitness()
+
+p2_2 = Population(10, 0.3, 0.2)
+p2_2.print_ids()
+p2_2.print_fitness()
+
+for x in range(20):
+    p2_2.next_genmo()
+    p2_2.print_ids()
+p2_2.print_fitness()
