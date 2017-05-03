0b1f391cd64f591bf1ad055a5faafe6349e481ec
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2017-05-03 10:55:31 +0900

    approaching #15

--- hamazaki1990/main.py
+++ hamazaki1990/main.py
@@ -6,19 +6,26 @@ from population import Population
 
 
 def get_heterogeneity(Population):
-    for x in Population._inds:
-        if Population[0] != Population[x]:
+    Population_ids = [x.get_id() for x in Population._inds]
+    for x in range(1, len(Population_ids)):
+        if Population_ids[0] != Population_ids[x]:
             return True
             break
-        else:
-            return False
+    else:
+        return False
 
 
 def simulate_fixwf(Population):
-    if complete_fixation(Population):
-
+    cnt = 1
+    while get_heterogeneity(Population):
+        Population = Population.next_genwf()
+        cnt += 1
+        print(Population._inds)
+    else:
+        print(cnt)
 
 
 p1 = Population(5)
-p1.print_ids
-simulate_fixation(p1)
+p1.print_ids()
+print(get_heterogeneity(p1))
+simulate_fixwf(p1)
