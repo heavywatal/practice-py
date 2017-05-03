33328c877e70033739df5d0e0a019c132e1c603c
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2017-05-03 09:21:19 +0900

    #15 tochuu

--- hamazaki1990/main.py
+++ hamazaki1990/main.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""main script
-"""
-print('Hello, world!')
+import random
+from individual import Individual
+from population import Population
+
+
+def get_heterogeneity(Population):
+    for x in Population._inds:
+        if Population[0] != Population[x]:
+            return True
+            break
+        else:
+            return False
+
+
+def simulate_fixwf(Population):
+    if complete_fixation(Population):
+
+
+
+p1 = Population(5)
+p1.print_ids
+simulate_fixation(p1)
