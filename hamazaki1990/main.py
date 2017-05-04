a98092fb684aff366b209b9542b93987aae86846
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2017-05-04 15:01:49 +0900

    #15 solving

--- hamazaki1990/main.py
+++ hamazaki1990/main.py
@@ -23,15 +23,6 @@ def simulate_fixwf(Population):  # simulate fixation in Wrigft-Fisher model
         return time, winner_id
 
 
-def repeat_simwf(trials, n, mutantrate=0, s=0):
-    # repeat simulation in Wrigft-Fisher model
-    result = []
-    for x in range(trials):
-        population = Population(n, mutantrate, s)
-        result.append(simulate_fixwf(population))
-    return result
-
-
 def simulate_fixmo(Population):   # simulate fixation in Moran model
     time = 0
     while get_heterogeneity(Population):
@@ -40,6 +31,41 @@ def simulate_fixmo(Population):   # simulate fixation in Moran model
     else:
         winner_id = Population._inds[0]
         return time, winner_id
+
+
+class Repeat_simwf:
+    def __init__(self, trials, n, mutantrate=0, s=0):
+        self._firstinds = n
+        result = []
+        for x in range(trials):
+            population = Population(n, mutantrate, s)
+            result.append(simulate_fixwf(population))
+        self._fixtime_wf = [result[x][0] for x in range(len(result))]
+        self._fixid_wf = [result[x][1] for x in range(len(result))]
+
+    def get_fixtime_wf(self):
+        return self._fixtime_wf
+
+    def ave_fixtime_wf(self):
+        average = sum(self._fixtime_wf)/len(self._fixtime_wf)
+        print(average)
+
+    def var_fixtime_wf(self):
+        trials = len(self._fixtime_wf)
+        a = sum(self._fixtime_wf)/trials
+        sqd = [(self._fixtime_wf[x] - a)**2 for x in range(trials)]
+        variance = sum(sqd)/trials
+        print(variance)
+
+    def get_fixid_wf(self):
+        return self._fixid_wf
+
+    def fixprob_wf(self):
+        n = self._firstinds
+        trials = len(self._fixid_wf)
+        fixid = self._fixid_wf
+        fixprob = {x: fixid.count(x)/trials for x in range(n)}
+        print(fixprob)
 
 
 def repeat_simmo(trials, n, mutantrate=0, s=0):
@@ -51,11 +77,24 @@ def repeat_simmo(trials, n, mutantrate=0, s=0):
     return result
 
 
+test = [0, 0, 1, 2, 3]
+print(test.count(0))
+test2 = [test.count(x) for x in range(len(test))]
+print(test2)
+test3 = {x: test.count(x)/len(test) for x in range(len(test))}
+print(test3)
+
 p1 = Population(10, 0.1, 0.5)
 p1.print_ids()
 print(get_heterogeneity(p1))
 print(simulate_fixwf(p1))
-print(repeat_simwf(5, 10, 0.1, 0.5))
+
+trial1 = Repeat_simwf(5, 10)
+print(trial1.get_fixtime_wf())
+trial1.ave_fixtime_wf()
+trial1.var_fixtime_wf()
+print(trial1.get_fixid_wf())
+trial1.fixprob_wf()
 
 p2 = Population(10, 0.1, 0.5)
 p2.print_ids()
