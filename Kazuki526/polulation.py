8348006c32c65706039fdeaf79457b1757b0c01a
Kazuki Takahashi <Kazuki526@users.noreply.github.com>
2017-05-10 18:11:06 +0900

    do issue #9 (#27)
    * do issue #9
    
    * Solve E501; use `.id` instead of `._id`

--- Kazuki526/polulation.py
+++ Kazuki526/polulation.py
@@ -1,4 +1,6 @@
 #! /usr/local/bin/env python
+# -*- coding: utf-8 -*-
+import random
 
 
 class Individual:
@@ -8,6 +10,9 @@ class Individual:
     @property
     def id(self):
         return(self._id)
+
+    def __repr__(self):
+        return(str(self._id))
 
 
 class Population:
@@ -21,6 +26,21 @@ class Population:
             print(individual.id, end=' ')
         print('')
 
+    def print_id(self):
+        print(self._individuals)
+
+    def wright_fisher_model(self):
+        next_gen = []
+        for pn in range(len(self._individuals)):
+            next_gen.append(random.choice(self._individuals))
+        self._individuals = sorted(next_gen, key=lambda ind: ind.id)
+
 
 sample_population = Population(10)
-sample_population.print_id_list()
+sample_population.print_id()
+print("go to next generation")
+sample_population.wright_fisher_model()
+sample_population.print_id()
+print("go to next generation again")
+sample_population.wright_fisher_model()
+sample_population.print_id()
